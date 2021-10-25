722509c01d3d1316b598bceb32e52d292bb24f6a
CaroAhn <ahncaro93@gmail.com>
2021-10-25 15:40:39 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>HOLA BOLA</h1>
+      <div className="container">
+        <h1>HOLA BOLA</h1>
+      </div>
     </div>
   );
 }
-
-export default App;
