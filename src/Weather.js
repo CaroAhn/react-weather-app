be47f30553e84b3141417d0da589910d9858142a
CaroAhn <ahncaro93@gmail.com>
2021-10-25 18:32:01 -0400

    Updated the search form

--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,18 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-control"
-        />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
       <h2>Atlanta</h2>
       <h4>Monday 25th - 17:00</h4>
@@ -21,11 +27,14 @@ export default function Weather() {
       <h1>55°C</h1>
       <h5>Cloudy</h5>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <p>Wind: 18 km/h</p>
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <p>Humidity: 70%</p>
+        </div>
+        <div className="col-4">
+          <p>Precipitacion: 15%</p>
         </div>
       </div>
     </div>
